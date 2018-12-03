ba8ecdd398efb6453731899abe4509800aa78e44
Arthur Araujo <arthurcruz.arj@gmail.com>
2018-12-03 17:52:21 -0500

    Models trained with grid based distance feature

--- utils/utils_line_supercover.py
+++ utils/utils_line_supercover.py
@@ -4,8 +4,15 @@ np.seterr(divide='ignore', invalid='ignore')
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
+# Limiting coordinates for NYC
+LAT_NORTH = 40.917577
+LAT_SOUTH = 40.477399 
+LON_EAST = -73.700272
+LON_WEST = -74.259090 
+
 class GridMap():
     
+    # Class intended for scaling
     class LatLonScaler():
         def fit(self, size, lat_north, lat_south, lon_east, lon_west):
             self.p_max = np.array((lon_east, lat_north))
@@ -22,60 +29,11 @@ class GridMap():
         self.vsize = size[1]
         self.scaler = self.LatLonScaler()
         self.scaler.fit(size, *bbox)
-        
-    def plot_path(self, p1, p2, need_norm = True, cells = None):
-        if need_norm == True:
-            p1 = self.scaler.transform(p1)
-            p2 = self.scaler.transform(p2)
-        
-        fig = plt.figure(figsize=(4, 4 * self.vsize / self.hsize))
-        ax = fig.add_subplot(1,1,1)
-
-        if cells is not None:
-            ax = fig.gca()
-            for cell in cells:
-                ax.add_patch(Rectangle((cell[0],cell[1]),1,1, facecolor='k', edgecolor='k',alpha=0.25))
-        
-        plt.scatter(p1[0], p1[1],c='r')
-        plt.scatter(p2[0], p2[1],c='g')
-        plt.plot([p1[0], p2[0]], [p1[1], p2[1]], c='k')
-        
-        ax.set_xticks(range(0, self.hsize + 1))
-        ax.set_yticks(range(0, self.vsize + 1))
-        ax.tick_params(labelleft=False, labelbottom=False, left=False, bottom=False)    
-
-        plt.xlim(0, self.hsize)
-        plt.ylim(0, self.vsize)
-        plt.grid()
-        plt.show()
-        
-    def plot_m_heat(self, m_map):
-        fig = plt.figure(figsize=(5, 5 * self.vsize / self.hsize))
-        ax = fig.add_subplot(1, 1, 1)
-
-        r,c = m_map.shape
-        for i in range(r):
-            for j in range(c):
-                ifm_map[i,j]
-                
-        print(median_map.shape)
-        #         if cells is not None:
-#             ax = fig.gca()
-#             for cell in cells:
-#                 ax.add_patch(Rectangle((cell[0],cell[1]),1,1, facecolor='k', edgecolor='k',alpha=0.25))
-#        
-#         plt.scatter(p1[0], p1[1],c='r')
-#         plt.scatter(p2[0], p2[1],c='g')
-#         plt.plot([p1[0], p2[0]], [p1[1], p2[1]], c='k')
-        
-        ax.set_xticks(range(0, self.hsize + 1))
-        ax.set_yticks(range(0, self.vsize + 1))
-        plt.xlim(0, self.hsize)
-        plt.ylim(0, self.vsize)
-        plt.grid()
-        plt.show()    
-        
-    def getSuperCover(self, p1, p2, need_norm=True):
+        self.grid_model = None
+    
+### Private methods
+    
+    def _getSuperCover(self, p1, p2, need_norm=True):
         # Implemented based on: http://playtechs.blogspot.com/2007/03/raytracing-on-grid.html
         
         if need_norm == True:
@@ -143,23 +101,62 @@ class GridMap():
     def _mapDataSampleToGrid(self, sample, assigning_map):
         pu = np.array([sample.pu_lon, sample.pu_lat])
         do = np.array([sample.do_lon, sample.do_lat])
-        covered_cells = self.getSuperCover(pu,do)
+        covered_cells = self._getSuperCover(pu,do)
         for cell in covered_cells:
             key = (cell[0],cell[1])
             if key in assigning_map:
                 assigning_map[(cell[0],cell[1])].append(sample.name)
             else:
                 assigning_map[(cell[0],cell[1])] = [sample.name]
-        return len(covered_cells)
+        return len(covered_cells)    
+    
+### Public methods
+    def plot_path(self, p1, p2, need_norm = True, show_cells = True):
+        if need_norm == True:
+            p1 = self.scaler.transform(p1)
+            p2 = self.scaler.transform(p2)
+        
+        fig = plt.figure(figsize=(4, 4 * self.vsize / self.hsize))
+        ax = fig.add_subplot(1,1,1)
 
-    def mapDataToGrid(self, dataset):
-        # This method assigns each sample to the grid cells for which that trip passes through
+        if show_cells is True:
+            ax = fig.gca()
+            cells = self._getSuperCover(p1, p2)
+            for cell in cells:
+                ax.add_patch(Rectangle((cell[0],cell[1]),1,1, facecolor='k', edgecolor='k',alpha=0.25))
+        
+        plt.scatter(p1[0], p1[1],c='r')
+        plt.scatter(p2[0], p2[1],c='g')
+        plt.plot([p1[0], p2[0]], [p1[1], p2[1]], c='k')
+        
+        ax.set_xticks(range(0, self.hsize + 1))
+        ax.set_yticks(range(0, self.vsize + 1))
+        ax.tick_params(labelleft=False, labelbottom=False, left=False, bottom=False)    
+
+        plt.xlim(0, self.hsize)
+        plt.ylim(0, self.vsize)
+        plt.grid()
+        plt.show()
+
+    def fitGrid(self, dataset, func):
+        # This method performs two steps:
+        #  1) Assigns each sample to the grid cell(s) through which that trip passes
+        #  2) For each cell, compute func through all samples assigned to that cell
+        
         assigning_map = {}
         sample_path_length = dataset.apply(lambda r : self._mapDataSampleToGrid(r, assigning_map), axis=1)
-        return assigning_map, sample_path_length
-    
-    def apply(self, dataset, assigning_map, func):
-        output = np.nan * np.empty((self.hsize, self.vsize))
+        
+        prior = getattr(dataset.trip_ratio, func)()
+        self.grid_model = prior * np.ones((self.hsize, self.vsize))
         for (i,j), value in assigning_map.items():
-            output[i][j] = getattr(dataset.loc[assigning_map[(i,j)]].trip_ratio, func)()
-        return output
+            self.grid_model[i][j] = getattr(dataset.loc[assigning_map[(i,j)]].trip_ratio, func)()
+        return self.grid_model
+
+    def _predictFactor(self, sample):
+        pu = np.array([sample.pu_lon, sample.pu_lat])
+        do = np.array([sample.do_lon, sample.do_lat])
+        cells = self._getSuperCover(pu, do)
+        return np.mean([self.grid_model[i][j] for (i,j) in cells])
+    
+    def predictFactor(self, data):
+        return data.apply(lambda s: self._predictFactor(s), axis=1)   
